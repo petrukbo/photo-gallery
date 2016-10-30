d55e39a834b8e82ebd5370817ae7dfad1866fd5a
Petya Raketa <svintkovdenis@mail.ru>
2016-10-30 20:55:54 +0200

    добавил возможность добавлять картинки

--- room.php
+++ room.php
@@ -3,6 +3,7 @@ session_start();
 include "User.php";
 $user = new User();
 if ($_SESSION['id']) $uid = $_SESSION['id']; else header("location:reg.php");
+if ($_SESSION['id'] == 1) header("location:admin.php");
 if (isset($_POST['submit1'])) $login = $user->login($_POST['s1'],$_POST['s2']);
 $l;$p;$e;$n;$c;
 
@@ -28,6 +29,11 @@ if (isset($_POST['exit']))
     session_destroy();
     header("location:index.php");
 }
+
+if (isset($_POST['addFile']))
+{
+    $path = $user->add_photo($_POST['image'],$_SESSION['id']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -44,7 +50,7 @@ if (isset($_POST['exit']))
 </head>
 <body background="ptrn1.jpg">
 <div id="main" align="center">
-    <!--****************************************************************************-->
+<!--****************************************************************************-->
     <div id="menu">
         <div id="pages">
             <a href="index.php">Home</a>|
@@ -52,7 +58,7 @@ if (isset($_POST['exit']))
             <a href="reg.php">Reg</a>|
             <a href="photo.php">Photo</a>
         </div>
-    <!--	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+<!--	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
         <?php if (!$_SESSION['id']) { ?>
             <form id = "sign_menu" method = "post" >
                 <input type = "text" name = "s1" placeholder = "Login" >
@@ -61,67 +67,60 @@ if (isset($_POST['exit']))
             </form >
         <?php } else { ?>
             <form id = "sign_menu" method = "post" >
-                <a href="room.php"><?php echo $user->getLogin($uid)?></a>
+                <?php if ($_SESSION['id'] != 1) { ?>
+                    <a href="room.php"><?php echo $user->getLogin($uid)?></a>
+                <?php } else {?>
+                    <a href="admin.php"><?php echo $user->getLogin($uid)?></a>
+                <?php } ?>
             </form >
         <?php } ?>
-    <!--	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+<!--	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
     </div>
-    <!--****************************************************************************-->
+<!--****************************************************************************-->
     <div id="content" align="center">
-        <form method="post">
-            <!-- <table>
-                <tr>
-                    <td>Name:</td>
-                    <td><?php echo $user->getName($uid)?></td>
-                </tr>
-                <tr>
-                    <td>Login:</td>
-                    <td><?php echo $user->getLogin($uid)?></td>
-                </tr>
-                <tr>
-                    <td>E-mail:</td>
-                    <td><?php echo $user->getEmail($uid)?></td>
-                </tr>
-                <tr>
-                    <td>Country:</td>
-                    <td><?php echo $user->getCountry($uid)?></td>
-                </tr>
-            </table> -->
-        <div id="inf_table">
-            <table>
-                <tr>
-                    <td>Name</td>
-                    <td><input type="text" name="name" placeholder="Name" value=<?php echo $user->getName($uid)?>></td>
-                </tr>
-                <tr>
-                    <td>Login</td>
-                    <td><input type="text" name="login" placeholder="Login" value=<?php echo $user->getLogin($uid)?>></td>
-                </tr>
-                <tr>
-                    <td>E-mail</td>
-                    <td><input type="text" name="email" placeholder="E-mail" value=<?php echo $user->getEmail($uid)?>></td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input type="password" name="password" placeholder="Password" value=<?php echo $user->getPassword($uid)?>></td>
-                </tr>
-                <tr>
-                    <td>Country</td>
-                    <td><input type="text" name="country" placeholder="Country" value=<?php echo $user->getCountry($uid)?>></td>
-                </tr>
-                <tr>
-                    <td><input id="editbut" type="submit" name="edit" value="Edit"></td>
-                </tr>
-            </table>
+        <form method="post" enctype="multipart/form-data">
+            <div id="inf_table">
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" name="name" placeholder="Name" value=<?php echo $user->getName($uid)?>></td>
+                    </tr>
+                    <tr>
+                        <td>Login</td>
+                        <td><input type="text" name="login" placeholder="Login" value=<?php echo $user->getLogin($uid)?>></td>
+                    </tr>
+                    <tr>
+                        <td>E-mail</td>
+                        <td><input type="text" name="email" placeholder="E-mail" value=<?php echo $user->getEmail($uid)?>></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="password" placeholder="Password" value=<?php echo $user->getPassword($uid)?>></td>
+                    </tr>
+                    <tr>
+                        <td>Country</td>
+                        <td><input type="text" name="country" placeholder="Country" value=<?php echo $user->getCountry($uid)?>></td>
+                    </tr>
+                    <tr>
+                        <td><input id="editbut" type="submit" name="edit" value="Edit"></td>
+                        <td><input type="file" name="image"></td>
+                        <td><input type="submit" name="addFile" value="Add File"></td>
+                    </tr>
+                </table>
             </div>
         </form>
     </div>
     <div id="img_content">
         <h3>Photos</h3>
-        <div><img src="img\1.jpg" alt=""></div>
-        <div><img src="img\2.jpg" alt=""></div>
-        <div><img src="img\3.jpg" alt=""></div>
+        <?php
+        $pictures = $user->get_picture($uid);
+        foreach($pictures as $pic)
+        {
+        ?>
+            <div><img src=<?php print $pic->path ?> alt=""></div>
+        <?php } ?>
     </div>
+
     <!--****************************************************************************-->
 </div>
 <div id="footer">
